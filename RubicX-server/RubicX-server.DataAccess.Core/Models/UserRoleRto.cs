769fa9efce44d18f514315181f591e634120eeff
rusumuhu <83660075+rusumuhu@users.noreply.github.com>
2021-11-14 17:54:22 +0300

    новый проект

---
+++ RubicX-server/RubicX-server.DataAccess.Core/Models/UserRoleRto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RubicX_server.DataAccess.Core.Models
+{
+    [Table("UserRole")]
+    public class UserRoleRto
+    
+    {
+       [Key] public int Id {get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
